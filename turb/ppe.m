0188479ac671b2ae011fbf9dd137670621077ee3
qingxu-thu <71426447+qingxu-thu@users.noreply.github.com>
2021-07-08 23:43:51 +0800

    Delete turb directory

--- turb/ppe.m
+++
@@ -1,6 +0,0 @@
-a= xlsread("C:\Users\Thinkpad\Desktop\新建 Microsoft Excel 工作表.xlsx");
-time1 = a(:,1);
-T1 = a(:,2);
-plot(time1,T1,'k','LineWidth',0.63)
-ylabel('MAX(T)/k');
-xlabel('time/s');
--- turb/ufd.m
+++
@@ -1,4 +0,0 @@
-t=0:pi/8:2*pi; %×î¼ò
-z=sawtooth(t,0.5);
-b=0:pi/8:15*pi
-plot(b,z);
